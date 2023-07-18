c0f14813b92b44c282ee641aada0720c0343643a
Vivecboots <Blairwin05@gmail.com>
2023-07-17 21:04:21 -0700

    add radial bar chart?

--- src/lib/TxTables2.svelte
+++ src/lib/TxTables2.svelte
@@ -1,11 +1,11 @@
 <script>
-//// src/lib TxTables2.svelte
   import { onMount, onDestroy } from 'svelte';
   import TronWeb from 'tronweb';
+  import { LinkedChart, LinkedLabel, LinkedValue } from "svelte-tiny-linked-charts"
 
-  let transactions = [];
+  let block = null;
   let loading = true;
-  let txHash = '';
+  let data = {};
 
   const HttpProvider = 'https://api.trongrid.io';
   const privateKey = import.meta.env.VITE_APP_PRIVATE_KEY; // Vite injects env variables with "VITE_" prefix
@@ -18,20 +18,15 @@
 
   const fetchLatestBlock = async () => {
     try {
-      const latestBlock = await tronWeb.trx.getCurrentBlock();
-      txHash = latestBlock.blockID;
-      transactions = latestBlock.transactions.map(transaction => {
-        const halfLength = Math.ceil(transaction.txID.length / 2);
-        const lastHalf = transaction.txID.substring(halfLength);
-        const fromAddress = transaction.raw_data.contract[0].parameter.value.owner_address;
-        return {
-          txID: lastHalf,
-          from: fromAddress
-        };
-      });
+      block = await tronWeb.trx.getCurrentBlock();
+      if (block.transactions) {
+        block.transactions.forEach(transaction => {
+          data[transaction.txID] = transaction.raw_data.contract[0].parameter.value.amount;
+        });
+      }
       loading = false;
     } catch (error) {
-      console.error('Error fetching transactions:', error);
+      console.error('Error fetching block:', error);
       loading = false;
     }
   };
@@ -40,7 +35,7 @@
 
   onMount(() => {
     fetchLatestBlock();
-    intervalId = setInterval(fetchLatestBlock, 600); // Fetch every 2 seconds
+    intervalId = setInterval(fetchLatestBlock, 900); // Refresh every 0.9 seconds
   });
 
   onDestroy(() => {
@@ -52,22 +47,48 @@
   <p>Loading...</p>
 {:else}
   <div class="container items-center">
-    <h2 class="text-xl font-bold mb-4">{txHash}</h2>
-    <table class="w-full table-auto">
-      <thead>
-        <tr>
-          <th class="px-4 py-2">Transaction Hash</th>
-          <th class="px-4 py-2">From Address</th>
-        </tr>
-      </thead>
-      <tbody>
-        {#each transactions as transaction}
-          <tr>
-            <td>{transaction.txID}</td>
-            <td class="text-right">{transaction.from}</td>
-          </tr>
-        {/each}
-      </tbody>
-    </table>
+    <h2 class="text-xl font-bold mb-4">Latest Tron Block</h2>
+    <div class="grid-container">
+      <div class="grid-item">
+        <p class="label">Block Height:</p>
+        <p>{block.block_header.raw_data.number}</p>
+      </div>
+      <div class="grid-item">
+        <p class="label">Number of Transactions:</p>
+        <p>{block.transactions.length}</p>
+      </div>
+      <div class="grid-item">
+        <p class="label">Block Date:</p>
+        <p>{new Date(block.block_header.raw_data.timestamp).toLocaleDateString()}</p>
+      </div>
+      <div class="grid-item">
+        <p class="label">Block Time:</p>
+        <p>{new Date(block.block_header.raw_data.timestamp).toLocaleTimeString()}</p>
+      </div>
+      <div class="grid-item">
+        {#if Object.keys(data).length > 0}
+          <LinkedChart {data} />
+        {/if}
+      </div>
+    </div>
   </div>
 {/if}
+
+<style>
+  .grid-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 10px;
+  }
+
+  .grid-item {
+    border: 1px solid black;
+    box-shadow: 2px 2px 4px #000;
+    padding: 10px;
+    background: transparent;
+  }
+
+  .label {
+    font-weight: bold;
+  }
+</style>
