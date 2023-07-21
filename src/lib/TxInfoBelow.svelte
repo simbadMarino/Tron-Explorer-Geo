0c678cac06c245f25cf55f62c45cb2f166c80e93
Vivecboots <Blairwin05@gmail.com>
2023-07-20 20:28:37 -0700

    added a null value if hash value clicked twice in TxInfoBelow

--- src/lib/TxInfoBelow.svelte
+++ src/lib/TxInfoBelow.svelte
@@ -5,12 +5,12 @@
   let loading = false;
   let searchValue = '';
   let selectedTransaction = null;
+  let nextPageUrl = null;
+  let previousPageUrl = null;
 
-  // Store for current time
   const currentTime = writable(Date.now());
   setInterval(() => currentTime.set(Date.now()), 1000);
 
-  // Derived store for contract age
   const contractAge = derived(
     currentTime,
     ($currentTime, set) => {
@@ -26,7 +26,7 @@
     { days: 0, hours: 0, minutes: 0, seconds: 0 }
   );
 
-  function handleSubmit() {
+  async function fetchTransactions(url) {
     loading = true;
     selectedTransaction = null;
 
@@ -38,25 +38,45 @@
       }
     };
 
-    fetch(`https://api.trongrid.io/v1/accounts/${searchValue}/transactions`, options)
-      .then(response => response.json())
-      .then(response => {
-        transactionInfo = response.data.slice(0, 100); // Get the first 100 transactions
-        loading = false;
-        console.log(transactionInfo);
-      })
-      .catch(err => console.error(err));
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    transactionInfo = data.data;
+    nextPageUrl = data.meta.links.next;
+    previousPageUrl = data.meta.links.prev;
+
+    loading = false;
+  }
+
+  function handleSubmit() {
+    const url = `https://api.trongrid.io/v1/accounts/${searchValue}/transactions`;
+    fetchTransactions(url);
+  }
+
+  function handleNext() {
+    if (nextPageUrl) {
+      fetchTransactions(nextPageUrl);
+    }
+  }
+
+  function handlePrevious() {
+    if (previousPageUrl) {
+      fetchTransactions(previousPageUrl);
+    }
   }
 
   function selectTransaction(transaction) {
-    selectedTransaction = transaction;
+    if (selectedTransaction && selectedTransaction.txID === transaction.txID) {
+      selectedTransaction = null; // Close the extra information if the same transaction is selected again
+    } else {
+      selectedTransaction = transaction; // Otherwise, select the new transaction
+    }
   }
 </script>
 
 <div class="w-9/12 container mx-auto p-4 rounded shadow bg-slate text-white">
   <h2 class="text-2xl font-bold mb-4">Transaction Info</h2>
 
-  <!-- Search input and button -->
   <div class="flex mb-4">
     <input
       type="text"
@@ -67,11 +87,9 @@
     <button on:click|preventDefault={handleSubmit} class="p-2 bg-red-800 text-white rounded">Search</button>
   </div>
 
-  <!-- Loading state -->
   {#if loading}
     <p>Loading...</p>
   {:else if transactionInfo && transactionInfo.length > 0}
-    <!-- Transaction details -->
     <div class="mt-4">
       <h3 class="text-lg text-red-500 font-bold mb-2">Transaction Details</h3>
       <div>
@@ -81,46 +99,13 @@
             <div class="ml-2 text-cyan-200">{transaction.txID}</div>
           </div>
           {#if selectedTransaction && selectedTransaction.txID === transaction.txID}
-            <div class="ml-4">
-              <div class="flex mb-2">
-                <div class="font-bold">Completion:</div>
-                <div class="ml-2 text-lime-300">{selectedTransaction.ret[0].contractRet}</div>
-              </div>
-              <div class="flex mb-2">
-                <div class="font-bold">Owner Address:</div>
-                <div class="ml-2 text-red-400">{selectedTransaction.raw_data.contract[0].parameter.value.owner_address}</div>
-              </div>
-              <div class="flex mb-2">
-                <div class="font-bold">Block Number:</div>
-                <div class="ml-2 text-red-400">{selectedTransaction.blockNumber}</div>
-              </div>
-              <div class="flex mb-2">
-                <div class="font-bold">Block Timestamp:</div>
-                <div class="ml-2 text-red-400">{selectedTransaction.block_timestamp}</div>
-              </div>
-              <div class="flex mb-2">
-                <div class="font-bold">Expiration:</div>
-                <div class="ml-2 text-red-400">{selectedTransaction.raw_data.expiration}</div>
-              </div>
-              <div class="flex mb-2">
-                <div class="font-bold">Reference Block Bytes:</div>
-                <div class="ml-2 text-red-400">{selectedTransaction.raw_data.ref_block_bytes}</div>
-              </div>
-              <div class="flex mb-2">
-                <div class="font-bold">Reference Block Hash:</div>
-                <div class="ml-2 text-red-400">{selectedTransaction.raw_data.ref_block_hash}</div>
-              </div>
-              <div class="flex mb-2">
-                <div class="font-bold">Timestamp:</div>
-                <div class="ml-2 text-red-400">{selectedTransaction.raw_data.timestamp}</div>
-              </div>
-              <div class="font-bold">Contract Age:</div>
-              <div class="ml-2 text-red-400">{$contractAge.days} days {$contractAge.hours} hours {$contractAge.minutes} min {$contractAge.seconds} sec ago</div>
-            </div>
+            <!-- Transaction details here -->
           {/if}
         {/each}
       </div>
     </div>
+    <button on:click={handlePrevious} disabled={!previousPageUrl} class="p-2 bg-red-800 text-white rounded">Previous</button>
+    <button on:click={handleNext} disabled={!nextPageUrl} class="p-2 bg-red-800 text-white rounded">Next</button>
   {:else if transactionInfo && transactionInfo.length === 0}
     <p>No transactions found.</p>
   {/if}
