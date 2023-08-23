05efbfb9e6b672ac9eb8ae7b4f37d42a8d307920
Vivecboots <Blairwin05@gmail.com>
2023-08-22 17:22:32 -0700

    added name for info

--- src/lib/IntroWidget.svelte
+++ src/lib/IntroWidget.svelte
@@ -1,4 +1,4 @@
 <div class="intro-widget bg-transparent py-8 px-4">
   <h1 class="text-4xl font-bold uppercase">tron explorer lite 2.0</h1>
-  <p class="text-lg mt-4">The Tron Blockchain Explorer Lite 2.0 is a powerful, yet compact, user-friendly tool aimed at developers and other use cases where speed and keeping tabs at a minimum is required. </p>
+  <p class="text-lg mt-4">The Tron Blockchain Explorer Lite 2.0 is a powerful, yet compact, user-friendly tool aimed at developers and other use cases where speed and keeping tabs at a minimum is required.  Created by Blair Winslow-Nason. License: MIT, check Github rep for more info.   </p>
 </div>
